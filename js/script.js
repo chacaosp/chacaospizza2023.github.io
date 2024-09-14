110ef173f10ff2a5c22f3d134619372e3f994ac6
chacaosp <chacaosp@gmail.com>
2024-09-14 17:24:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,43 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
 
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
 
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 
-  if(window.scrollY > 60){
+  if (window.scrollY > 60) {
     document.querySelector('#scroll-top').classList.add('active');
-  }else{
+  } else {
     document.querySelector('#scroll-top').classList.remove('active');
   }
 
 }
+//--------------------------------------------------------------------------------------------------------------
 
-function loader(){
+document.querySelector('.submenu > a').onclick = (e) => {
+  e.preventDefault(); // Evitar la navegación del enlace principal
+  document.querySelector('.submenu .dropdown').classList.toggle('active');
+};
+
+//--------------------------------------------------------------------------------------------------------------
+
+function loader() {
   document.querySelector('.loader-container').classList.add('fade-out');
 }
 
-function fadeOut(){
+function fadeOut() {
   setInterval(loader, 3000);
 }
 
 window.onload = fadeOut();
+
+
+
